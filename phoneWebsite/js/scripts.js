75eca8531f2264708bb0d397a33422b90bcb730c
nelskev <144828052+nelskev@users.noreply.github.com>
2024-02-05 14:26:17 -0700

    Add files via upload

---
+++ phoneWebsite/js/scripts.js
@@ -0,0 +1,3 @@
+// The current year is retrieved and is placed in the span tag with the id "year" in index.html
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
