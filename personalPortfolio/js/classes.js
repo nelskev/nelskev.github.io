4c29194638281b83138dd7c76a311d691259e0ac
nelskev <144828052+nelskev@users.noreply.github.com>
2025-01-14 21:11:46 -0700

    Update classes.js

--- personalPortfolio/js/classes.js
+++ personalPortfolio/js/classes.js
@@ -1,32 +1,30 @@
-const csvFile = '../data/classes.csv';
-
-
-function buildClassesTable() {
-  fetch(csvFile)
-    .then(response => response.text())
-    .then(data => {
-      const parsedData = Papa.parse(data, { download: true, header: true }).data;
-
-      const table = $('<table>');
-      table.append($('<tr>').addClass('noMobile')
-        .append($('<td>').text('Number'))
-        .append($('<td>').text('Name'))
-        .append($('<td>').text('Language')));
-
-      parsedData.forEach(row => {
-        const dataRow = $('<tr>');
-        dataRow.append($('<td>').text(row.Number));
-        dataRow.append($('<td>').text(row.Name));
-        dataRow.append($('<td>').text(row.Language));
-        table.append(dataRow);
-      });
-
-      $('main').append(table);
-    })
-    .catch(error => console.error('Error:', error));
-}
-
-// Call the function to build the table
 $(document).ready(function() {
+  const csvFile = '../data/classes.csv';
+
+  function buildClassesTable() {
+    fetch(csvFile)
+      .then(response => response.text())
+      .then(data => {
+        const parsedData = Papa.parse(data, { download: true, header: true }).data;
+
+        const table = $('<table>');
+        table.append($('<tr>').addClass('noMobile')
+          .append($('<td>').text('Number'))
+          .append($('<td>').text('Name'))
+          .append($('<td>').text('Language')));
+
+        parsedData.forEach(row => {
+          const dataRow = $('<tr>');
+          dataRow.append($('<td>').text(row.Number));
+          dataRow.append($('<td>').text(row.Name));
+          dataRow.append($('<td>').text(row.Language));
+          table.append(dataRow);
+        });
+
+        $('main').append(table);
+      })
+      .catch(error => console.error('Error:', error));
+  }
+
   buildClassesTable();
 });
