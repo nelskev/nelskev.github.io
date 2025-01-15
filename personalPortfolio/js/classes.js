f354e02ec76ff9d9cc66922903bc813fd6b31f68
nelskev <144828052+nelskev@users.noreply.github.com>
2025-01-15 15:06:58 -0700

    Changed classes to remove static table rows, index to add fancy background from particle.js, index.css to help organize it, and classes.js to add to table not remove it

--- personalPortfolio/js/classes.js
+++ personalPortfolio/js/classes.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   const csvFile = '../data/classes.csv';
+  const $existingTable = $('table.classTable'); 
 
   function buildClassesTable() {
     fetch(csvFile)
@@ -8,24 +9,15 @@ $(document).ready(function() {
         // Split the CSV data into rows
         const rows = data.trim().split('\n');
 
-        // Create the table
-        const table = $('<table>');
-        table.append($('<tr>').addClass('noMobile')
-          .append($('<td>').text('Number'))
-          .append($('<td>').text('Name'))
-          .append($('<td>').text('Language')));
-
         // Iterate through the rows and create table cells
-        for (let i = 1; i < rows.length; i++) {
+        for (let i = 0; i < rows.length; i++) {
           const cells = rows[i].split(',');
           const dataRow = $('<tr>');
           dataRow.append($('<td>').text(cells[0]));
           dataRow.append($('<td>').text(cells[1]));
           dataRow.append($('<td>').text(cells[2]));
-          table.append(dataRow);
+          $existingTable.append(dataRow);
         }
-
-        $('main').append(table);
       })
       .catch(error => console.error('Error:', error));
   }
